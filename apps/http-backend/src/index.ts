82c277928f75e1a6f0d16af8fd2741795c12fec5
Hemanth sai Somaraju <hemanthsaisomaraju@gmail.com>
2025-07-07 19:44:51 +0530

    added prisma ,added schema, ran init migrations, ran prisma generate to make prisma client accessible and exported it via src/index.ts

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,6 +3,7 @@ import { JWT_SECRET } from "@repo/backend-common/config";
 import jwt from 'jsonwebtoken'
 import { middleware } from './middleware';
 import {CreateRoomSchema, CreateUserSchema, SigninSchema} from "@repo/common/types";
+import {prismaClient} from "@repo/db/client";
 const app=express();
 app.use(express.json());
  
