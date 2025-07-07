d647ffcaf3a0c7f61db2778803ed47fab49cc871
Hemanth sai Somaraju <hemanthsaisomaraju@gmail.com>
2025-07-07 14:09:04 +0530

    added basic backend code ,added backend common,common, db packages in the packages and used into backend (configured package.json to use common codebase)

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,43 @@
 import express from 'express'
+import { JWT_SECRET } from "@repo/backend-common/config";
+import jwt from 'jsonwebtoken'
+import { middleware } from './middleware';
+import {CreateRoomSchema, CreateUserSchema, SigninSchema} from "@repo/common/types";
 const app=express();
 app.use(express.json());
+ 
+app.post('/signup',(req,res)=>{
+    //zod validation
+    const data=CreateUserSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({message:"Incorrect inputs"})
+    }
+    //db calls
+    res.json({message:"signed up"});
+});
+app.post('/signin',(req,res)=>{
+    const {username,password}=req.body;
+    // zod validation and db call
+    const data=SigninSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({message:"Incorrect inputs"});
+    }
+    const userId=1;
+    const token=jwt.sign({
+        userId
+    },JWT_SECRET);
+    res.json({token});
+});
+app.post('/create-room',middleware,(req,res)=>{
+    // db call
+    const data=CreateRoomSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({message:"Incorrect inputs"});
+    }
+    res.json({
+        roomId:123123 // should be random
+    })
+});
+
 
 app.listen(3005);
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,7 +1,11 @@
 import { WebSocketServer } from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
 const wss=new WebSocketServer({port:8080});
-wss.on("connection",function connection(ws){
+wss.on("connection",function connection(ws,request){
+    const token=request.headers.authorization??"";
+    const user=jwt.verify(token,JWT_SECRET) as JwtPayload;
     ws.on("message",function message(data){
-        ws.send(data);
+        ws.send(data+"\n****\n"+user.userId);
     })
 })
